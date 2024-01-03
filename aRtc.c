38132a102c300befcdacfd0f4b6f812ef1588cc5
Pablo Jean Rozario <pablo.jean.eel@gmail.com>
2024-01-02 21:58:09 -0300

    fixed the 40 years increase life-time enhancment

--- aRtc.c
+++ aRtc.c
@@ -15,8 +15,8 @@
 void aRtc_set(RTC_HandleTypeDef *hrtc, struct tm Time){
 	time_t utc;
 
-#if (ARTC_INCREASE_80_YEARS == 1)
-	Time.tm_year -= 80;
+#if (ARTC_INCREASE_40_YEARS == 1)
+	Time.tm_year -= 40;
 #endif
 	utc = mktime(&Time);
 
@@ -36,8 +36,8 @@ void aRtc_get(RTC_HandleTypeDef *hrtc, struct tm *Time){
 
 	local = localtime(&utc);
 
-#if (ARTC_INCREASE_80_YEARS == 1)
-	Time->tm_year += 80;
+#if (ARTC_INCREASE_40_YEARS == 1)
+	local->tm_year += 40;
 #endif
 
 	*Time = *local;
